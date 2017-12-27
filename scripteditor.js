ed05fb0f05f7bf64285b3b69f710d77b675a5e86
Ryan Moise <rmoiser_13@hotmail.com>
2017-12-27 15:25:46 -0500

    Added BTC function
    Changes the name of the getRange function.
    Added a function to grab the BTC prices. Changed the ticker variables to Arrays to allow for USD and BTC rate storage.

--- scripteditor.js
+++ scripteditor.js
@@ -13,10 +13,10 @@ function getData() {
   //Change the name in the quotes (e.g. are-bees-carebears) to match the 'id' field from https://api.coinmarketcap.com/v1/ticker/
   //Copy/paste to add more lines as needed
   
-  var ABC = getRate('are-bees-carebears');
-  var BCD = getRate('berry-cool-doge');
-  var CDE = getRate('coin-dank-enigma');
-  
+  var ABC = [getUSDRate('are-bees-carebears'), getBTCRate('are-bees-carebears')];
+  var BCD = [getUSDRate('berry-cool-doge'), getBTCRate('berry-cool-doge')];;
+  var CDE = [getUSDRate('coin-dank-enigma'), getBTCRate('coin-dank-enigma')];;
+
   //Grabbing values that are on CoinMarketCap but not in the API
   //Change the variable name to match the trading symbol
   //Go to the CoinMarketCap page for the currency (e.g. https://coinmarketcap.com/currencies/zeeyx)
@@ -31,9 +31,12 @@ function getData() {
   //Change the values in getRange() to match the cells in the 'Rates' sheet you want to userAgent
   //Use the coin symbols from above in setValue()
 
-  ssRates.getRange('B1').setValue(ABC);
-  ssRates.getRange('B2').setValue(BCD);
-  ssRates.getRange('B3').setValue(CDE);
+  ssRates.getRange('B4').setValue(ABC[0]);
+  ssRates.getRange('C4').setValue(ABC[1]);
+  ssRates.getRange('B4').setValue(BCD[0]);
+  ssRates.getRange('C4').setValue(BCD[1]);
+  ssRates.getRange('B4').setValue(CDE[0]);
+  ssRates.getRange('C4').setValue(CDE[1]);
   
   ssRates.getRange('B4').setValue(ZYX);
   ssRates.getRange('B5').setValue(YXW);
@@ -84,7 +87,7 @@ function getVtcBalance(vtcAddress) {
   return obj;
 }
 
-function getRate(currencyId) {
+function getUSDRate(currencyId) {
 
   var url = 'https://api.coinmarketcap.com/v1/ticker/' + currencyId + '/';
   var response = UrlFetchApp.fetch(url, {'muteHttpExceptions': true});
@@ -92,6 +95,16 @@ function getRate(currencyId) {
   var data = JSON.parse(json);
 
   return parseFloat(data[0]['price_usd']);
+}
+
+function getBTCRate(currencyId) {
+
+  var url = 'https://api.coinmarketcap.com/v1/ticker/' + currencyId + '/';
+  var response = UrlFetchApp.fetch(url, {'muteHttpExceptions': true});
+  var json = response.getContentText();
+  var data = JSON.parse(json);
+
+  return parseFloat(data[0]['price_btc']);
 }
 
 function getWebRate(currencyId) {
